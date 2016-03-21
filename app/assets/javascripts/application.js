61d371c412c534fa0e2a1e3922ff110698a756f2
Justin Pease <justinpease2@gmail.com>
2016-03-21 17:32:45 -0600

    ideas are now appended to the page as the result of a successful ajax call

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,36 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function(){
+  getIdeas();
+});
+
+function getIdeas(){
+  $.ajax({
+    url: '/api/v1/ideas',
+    type: 'GET',
+    success: function(response){
+      displayIdeas(response);
+      console.log(response)
+    }, error: function(xhr) {
+      console.log("uhh ohh!!!")
+    }
+  })
+}
+
+function displayIdeas(ideas){
+  ideas.forEach(function(idea){
+    $('#idea-index').append(
+      '<div class="panel panel-default">' +
+      '<div class="panel-heading">' +
+        '<h3 class="panel-title">Title: ' + idea.title + '</h3><br>' +
+        '<p>Idea Quality: ' + idea.quality + '</p>' +
+      '</div>' +
+      '<div class="panel-body">' +
+        idea.body +
+      '</div>' +
+    '</div>'
+    )
+  });
+}
