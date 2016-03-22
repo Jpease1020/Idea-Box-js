45824e066863786a62ef63551c1fd04a0e6ad0ea
Justin Pease <justinpease2@gmail.com>
2016-03-22 17:49:40 -0600

    the body of an idea is content editable and the data for the idea body changes in the database

--- app/assets/javascripts/edit_ideas.js
+++ app/assets/javascripts/edit_ideas.js
@@ -3,12 +3,13 @@ function editIdeas(){
     var $ideaDataId = $(this).children('span').attr('data-id')
     var $ideaId = $(this).closest('.idea').attr('data-id')
     var $ideaNewData = $(this).children('span').text()
-debugger
+
     if($ideaDataId == "title"){
       ideaParams = { 'title': $ideaNewData }
-    } else if(ideaDataId == "body"){
+    } else if($ideaDataId == "body"){
       ideaParams = { 'body': $ideaNewData }
     }
+
     $.ajax({
       url: 'api/v1/ideas/' + $ideaId,
       type: 'PUT',
