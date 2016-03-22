ca24e78dbafab085710057f0b71c0bc4417e2083
Justin Pease <justinpease2@gmail.com>
2016-03-22 01:03:57 -0600

    changed the quality of an idea from a string in the database to the enum role method

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321200234) do
+ActiveRecord::Schema.define(version: 20160322065730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "ideas", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "title"
     t.string   "body"
-    t.string   "quality"
+    t.integer  "quality",    default: 0
   end
 
 end
