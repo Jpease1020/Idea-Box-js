7ba405085d20690466d7779af10b9cc66bb92516
Justin Pease <justinpease2@gmail.com>
2016-03-22 17:47:26 -0600

    the titel is currently content editable and the new title gets stored in the database

--- app/assets/javascripts/edit_ideas.js
+++ app/assets/javascripts/edit_ideas.js
@@ -1,5 +1,23 @@
 function editIdeas(){
-  $('#idea-index').delegate('.idea-body', 'blur', function(){
-    alert("This input field has lost its focus.");
-    });
+  $('#idea-index').delegate('.edit-idea', 'blur', function(){
+    var $ideaDataId = $(this).children('span').attr('data-id')
+    var $ideaId = $(this).closest('.idea').attr('data-id')
+    var $ideaNewData = $(this).children('span').text()
+debugger
+    if($ideaDataId == "title"){
+      ideaParams = { 'title': $ideaNewData }
+    } else if(ideaDataId == "body"){
+      ideaParams = { 'body': $ideaNewData }
+    }
+    $.ajax({
+      url: 'api/v1/ideas/' + $ideaId,
+      type: 'PUT',
+      data: ideaParams,
+      success: function(response){
+        console.log('good')
+      }, error: function(xhr){
+        console.log(xhr.responseText)
+      }
+    })
+  })
 }
