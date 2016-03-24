f3bb5807bfd17e4a4f63a0fe1c1a5a249a5ba3ab
Justin Pease <justinpease2@gmail.com>
2016-03-23 19:12:47 -0600

    fixed search when deleting text in the text box

--- app/assets/javascripts/search_idea.js
+++ app/assets/javascripts/search_idea.js
@@ -2,12 +2,13 @@ function searchIdeas(){
   $('#idea-searcher').on('keyup', function(){
     var inputText = $(this).children('input').val().toLowerCase();
     var ideaElements = $('.idea')
+
+    ideaElements.removeClass('hidden')
     var hideIdeas = ideaElements.filter(function(){
       ideaBodyAndText = $(this).find(".title").text().toLowerCase() +
           $(this).find(".body").text().toLowerCase()
       return !(ideaBodyAndText.includes(inputText))
     })
-    // $('.idea').addClass('hidden')
     hideIdeas.addClass('hidden')
   })
 }
