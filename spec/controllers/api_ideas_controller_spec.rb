da6bf17f1bc66b3fe2b4d2a37f4dad5b30652879
Justin Pease <justinpease2@gmail.com>
2016-03-24 00:11:01 -0600

    adds tests for update and delete but they can not find the action

--- spec/controllers/api_ideas_controller_spec.rb
+++ spec/controllers/api_ideas_controller_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Api::V1::IdeasController, :type => :controller do
   describe "Ideas controller" do
     describe "index" do
-      it "home page is the root path" do
+      it "index returns ideas" do
         idea = create(:idea)
 
         get "index", format: :json
@@ -15,10 +15,36 @@ RSpec.describe Api::V1::IdeasController, :type => :controller do
     end
 
     describe "create" do
-      it "home page is the root path" do
+      it "creates and returns an idea" do
         idea = create(:idea, title: "a new title", body: "okok")
 
-        get "create", format: :json, title: idea.title
+        post "create", format: :json, title: idea.title
+
+        expect(response.status).to eq 201
+        ideas = JSON.parse(response.body)
+        expect(idea["title"]).to eq "a new title"
+        expect(idea["body"]).to eq "okok"
+      end
+    end
+
+    describe "update" do
+      it "updates and returns an idea" do
+        idea = create(:idea, title: "a new title", body: "okok")
+
+        put "update", format: :json, controller: "api/vi/ideas/#{idea.id}", id: idea.id, title: "changing it up"
+
+        expect(response.status).to eq 200
+        ideas = JSON.parse(response.body)
+        expect(idea["title"]).to eq "a new title"
+        expect(idea["body"]).to eq "okok"
+      end
+    end
+
+    describe "delete" do
+      it "deletes an idea" do
+        idea = create(:idea, title: "a new title", body: "okok")
+
+        delete "destroy", format: :json, controller: "api/vi/ideas/#{idea.id}", id: idea.id
 
         expect(response.status).to eq 200
         ideas = JSON.parse(response.body)
