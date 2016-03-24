6090c5d38c0f1069cb8c59faed7d687ec539cb5c
Justin Pease <justinpease2@gmail.com>
2016-03-24 09:07:49 -0600

    removes button for searching since the typing dynamically searches

--- spec/features/user_thumbs_up_an_idea_spec.rb
+++ spec/features/user_thumbs_up_an_idea_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "User thumbs up an idea", type: :feature do
-  xscenario "idea is created will default swill quality then upgraded", js: true do
+  scenario "idea is created will default swill quality then upgraded", js: true do
     visit root_path
 
     fill_in "new-idea-title", with: "My new idea title"
