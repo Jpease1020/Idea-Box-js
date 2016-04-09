9871caffc6eaab5b5f2e32db7e7411848561a55d
Justin Pease <justinpease2@gmail.com>
2016-04-09 14:01:31 -0600

    uses the get JSON method to shorten an ajax get request

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,19 +1,13 @@
 function getIdeas(){
-  $.ajax({
-    url: '/api/v1/ideas',
-    type: 'GET',
-    success: function(response){
+  $.getJSON('/api/v1/ideas', function(response){
       displayIdeas(response);
-    }, error: function(xhr) {
-      console.log(xhr.responseText)
-    }
-  })
+  });
 }
 
 function displayIdeas(ideas){
   ideas.forEach(function(idea){
-    idea.body = truncateIdeaBody(idea)
-    $('#idea-index').append(ideaHtml(idea))
+    idea.body = truncateIdeaBody(idea);
+    $('#idea-index').append(ideaHtml(idea));
   });
 }
 
@@ -21,10 +15,10 @@ function truncateIdeaBody(idea){
   if (idea.body.length > 100){
     var choppedBody = " " + idea.body.slice(0,102);
     for (i = choppedBody.length - 1; choppedBody[i] != " "; i--){
-        choppedBody = choppedBody.slice(0, i)
+        choppedBody = choppedBody.slice(0, i);
     }
-    return choppedBody.slice(0, -1) + "..."
+    return choppedBody.slice(0, -1) + "...";
   } else {
-    return idea.body
+    return idea.body;
   }
 }
